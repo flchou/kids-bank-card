ac4d4f81feb49db2ebf4a2ec226b522eba4e2845
flchou <flchou@gmail.com>
2026-03-13 12:11:13 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,7 +47,7 @@ def generate_pro_bmp(worksheet, name):
         f_label = ImageFont.truetype(font_path, 11)
         f_section = ImageFont.truetype(font_path, 13)
         f_row_time = ImageFont.truetype(font_path, 14)
-        f_row_desc = ImageFont.truetype(font_path, 12)
+        f_row_desc = ImageFont.truetype(font_path, 14)
         f_sync = ImageFont.truetype(font_path, 12) # Increased font size
     except Exception as e:
         st.error(f"Font Error: {e}")
